eb9796d60c7368fc6953e4a3c20755a36c9647be
L3B3 <drodriguez1@babson.edu>
2025-11-12 15:01:00 -0500

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio-Manager
-Final Project on Managing and Analyzing Stock portfolio information through a python file
---
+++ main.py
@@ -0,0 +1,10 @@
+#Import ALL the functions and Variables Neccessary
+# Ex: from "__File_Name__" import "Function_Name_1,Function_Name_2,Function_Name_3... "
+
+"""In this file only the main() function is allowed, as such it is the defining function that puts together all the
+other files."""
+
+def main():
+    print("Main is running properly")
+if __name__ == "__main__":
+    main()
